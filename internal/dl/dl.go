02cb482b882ca43e93818f3a8790c4bd4db35b1a
Roman Timashev <roman@tmshv.ru>
2025-05-03 01:14:23 +0300

    improve dl; add retries

--- internal/dl/dl.go
+++ internal/dl/dl.go
@@ -1,6 +1,7 @@
 package dl
 
 import (
+	"errors"
 	"io"
 	"net/http"
 	"time"
@@ -14,19 +15,33 @@ type Downloader struct {
 }
 
 func (dl *Downloader) Download(url string) ([]byte, error) {
+	var e error
+	for range dl.Retries {
+		body, err := dl.dl(url)
+		if err != nil {
+			e = err
+			continue
+		}
+		return body, nil
+	}
+	return nil, e
+}
+
+func (dl *Downloader) dl(url string) ([]byte, error) {
 	req, _ := http.NewRequest("GET", url, nil)
-
-	resp, err := dl.client.Do(req)
+	res, err := dl.client.Do(req)
 	if err != nil {
 		return nil, err
 	}
+	if res.StatusCode != http.StatusOK {
+		return nil, errors.New("Not 200 OK")
+	}
 
-	// var body []byte
-	body, err := io.ReadAll(resp.Body)
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
 
 	return body, nil
 }
