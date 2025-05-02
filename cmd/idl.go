835bc18b27a1d1628f479a5ba5c659a5787f596c
Roman Timashev <roman@tmshv.ru>
2025-05-03 01:56:08 +0300

    graceful termination

--- cmd/idl.go
+++ cmd/idl.go
@@ -3,7 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 	"runtime"
+	"syscall"
 
 	"github.com/tmshv/idl/internal/config"
 	"github.com/tmshv/idl/internal/idl"
@@ -19,8 +22,15 @@ func main() {
 	cpu := runtime.NumCPU()
 	fmt.Printf("Number of CPUs: %d\n", cpu)
 
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() { // catch signal and invoke graceful termination
+		stop := make(chan os.Signal, 1)
+		signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+		<-stop
+		cancel()
+	}()
+
 	idl := idl.New(cpu)
-	ctx := context.Background()
 	err = idl.Run(ctx, cfg)
 	if err != nil {
 		panic(err)
