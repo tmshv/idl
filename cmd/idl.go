76b278e47a944936dd82f4538d9229c0057054c2
Roman Timashev <roman@tmshv.ru>
2025-05-03 01:18:49 +0300

    move from main to runner; improve download concurrency

--- cmd/idl.go
+++ cmd/idl.go
@@ -1,104 +1,28 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"path"
 	"runtime"
-	"sync"
 
-	"github.com/h2non/bimg"
 	"github.com/tmshv/idl/internal/config"
-	"github.com/tmshv/idl/internal/csv"
-	"github.com/tmshv/idl/internal/dl"
-	"github.com/tmshv/idl/internal/preprocess/compose"
-	"github.com/tmshv/idl/internal/preprocess/resize"
-	"github.com/tmshv/idl/internal/utils"
+	"github.com/tmshv/idl/internal/idl"
 )
-
-type image struct {
-	file string
-	data []byte
-}
 
 func main() {
 	cfg, err := config.Get()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%v\n", cfg)
+	fmt.Printf("Options: %+v\n", cfg)
 
 	cpu := runtime.NumCPU()
 	fmt.Printf("Number of CPUs: %d\n", cpu)
 
-	recordCh := make(chan csv.Record, cpu)
-	go (func() {
-		reader := csv.New(cfg.Fields.URL, cfg.Fields.File)
-		err := reader.Read(cfg.Input, recordCh)
-		if err != nil {
-			fmt.Printf("Failed to read CSV file: %v\n", err)
-		}
-	})()
-
-	dlCh := make(chan image, cfg.Workers)
-	go func() {
-		defer close(dlCh)
-		dl := dl.New(cfg.Timeout, 1)
-		wg := sync.WaitGroup{}
-		for rec := range recordCh {
-			wg.Add(1)
-			go func(wg *sync.WaitGroup) {
-				defer wg.Done()
-
-				file := path.Join(cfg.Dir, rec.File)
-				if !cfg.Reload && utils.FileExists(file) {
-					fmt.Printf("Skip: %s\n", file)
-					return
-				}
-
-				body, err := dl.Download(rec.URL)
-				if err != nil {
-					fmt.Printf("Failed to download image: %v\n", err)
-					return
-				}
-
-				dlCh <- image{file: file, data: body}
-			}(&wg)
-		}
-		wg.Wait()
-	}()
-
-	imgCh := make(chan image, cpu)
-	go func() {
-		defer close(imgCh)
-
-		prep := compose.New(
-			resize.New(cfg.Resize[0], cfg.Resize[1]),
-		)
-
-		wg := sync.WaitGroup{}
-		for range cfg.Workers {
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				for i := range dlCh {
-					i.data, err = prep.Run(i.data)
-					if err != nil {
-						fmt.Printf("Failed to preprocess image: %v\n", err)
-						return
-					}
-					imgCh <- i
-				}
-			}()
-		}
-		wg.Wait()
-	}()
-
-	for i := range imgCh {
-		err := bimg.Write(i.file, i.data)
-		if err != nil {
-			fmt.Printf("Failed to write image: %v\n", err)
-			continue
-		}
-		fmt.Printf("OK: %v\n", i.file)
+	idl := idl.New(cpu)
+	ctx := context.Background()
+	err = idl.Run(ctx, cfg)
+	if err != nil {
+		panic(err)
 	}
 }
---
+++ internal/idl/idl.go
@@ -0,0 +1,111 @@
+package idl
+
+import (
+	"context"
+	"fmt"
+	"path"
+	"sync"
+
+	"github.com/h2non/bimg"
+	"github.com/tmshv/idl/internal/config"
+	"github.com/tmshv/idl/internal/csv"
+	"github.com/tmshv/idl/internal/dl"
+	"github.com/tmshv/idl/internal/preprocess/compose"
+	"github.com/tmshv/idl/internal/preprocess/resize"
+	"github.com/tmshv/idl/internal/utils"
+)
+
+type image struct {
+	file string
+	data []byte
+}
+
+type IDL struct {
+	cpu int
+}
+
+func (idl *IDL) Run(ctx context.Context, cfg config.Config) error {
+	recordCh := make(chan csv.Record, idl.cpu)
+	go (func() {
+		reader := csv.New(cfg.Fields.URL, cfg.Fields.File)
+		err := reader.Read(cfg.Input, recordCh)
+		if err != nil {
+			fmt.Printf("Failed to read CSV file: %v\n", err)
+		}
+	})()
+
+	dlCh := make(chan image, cfg.Workers)
+	go func() {
+		defer close(dlCh)
+		dl := dl.New(cfg.Timeout, 1)
+		wg := sync.WaitGroup{}
+		for range cfg.Workers {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+
+				for rec := range recordCh {
+					file := path.Join(cfg.Dir, rec.File)
+					if !cfg.Reload && utils.FileExists(file) {
+						fmt.Printf("Skip: %s\n", file)
+						continue
+					}
+
+					body, err := dl.Download(rec.URL)
+					if err != nil {
+						fmt.Printf("Failed to download: %v\n", err)
+						continue
+					}
+
+					dlCh <- image{file: file, data: body}
+				}
+			}()
+		}
+
+		wg.Wait()
+	}()
+
+	imgCh := make(chan image, idl.cpu)
+	go func() {
+		defer close(imgCh)
+
+		prep := compose.New(
+			resize.New(cfg.Resize[0], cfg.Resize[1]),
+		)
+
+		wg := sync.WaitGroup{}
+		for range idl.cpu {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				for i := range dlCh {
+					data, err := prep.Run(i.data)
+					if err != nil {
+						fmt.Printf("Failed to preprocess image: %v\n", err)
+						return
+					}
+					i.data = data
+					imgCh <- i
+				}
+			}()
+		}
+		wg.Wait()
+	}()
+
+	for i := range imgCh {
+		err := bimg.Write(i.file, i.data)
+		if err != nil {
+			fmt.Printf("Failed to write image: %v\n", err)
+			continue
+		}
+		fmt.Printf("OK: %v\n", i.file)
+	}
+
+	return nil
+}
+
+func New(cpu int) *IDL {
+	return &IDL{
+		cpu: cpu,
+	}
+}
